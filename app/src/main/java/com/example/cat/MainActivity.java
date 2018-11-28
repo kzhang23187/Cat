7c28e2c5e0ef50bde8e6e6ad551ed9a92e5823bf
Kevin Zhang <kzhang23187@gmail.com>
2018-11-27 18:46:59 -0600

    UI changes

--- app/src/main/java/com/example/cat/MainActivity.java
+++ app/src/main/java/com/example/cat/MainActivity.java
@@ -33,9 +33,11 @@ public class MainActivity extends AppCompatActivity{
 
         squareSide = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, r.getDisplayMetrics());
         margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 0, r.getDisplayMetrics());
+        ImageView first = findViewById(R.id.imageView3);
+        board[1][1] = first;
+
 
         //What to do with each button click
-        //Check if all tiles are full, if so then display end game score as a toast and reset board
         //
         //Move all tiles in a direction in the array
         //and check if the contentDescription are equal if so then make new tile where the two will go and "combine them"
@@ -43,29 +45,36 @@ public class MainActivity extends AppCompatActivity{
         //Add reference to that tile into the array for logical purposes
         //Increment score by 9 everytime a combination occurs
         //Display a new cat fact through API call to catfact thing everytime two squares combine
+        //Check if all tiles are full, if so then display end game score as a toast and reset board
+        //
         //
         //
         findViewById(R.id.up).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
+                moveTiles(board, 1);
+
 
             }
         });
         findViewById(R.id.down).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
+                moveTiles(board, 2);
 
             }
         });
         findViewById(R.id.right).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
+                moveTiles(board, 3);
 
             }
         });
         findViewById(R.id.left).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
+                moveTiles(board, 4);
 
             }
         });
@@ -80,11 +89,25 @@ public class MainActivity extends AppCompatActivity{
     }
 
     /**
+     * resets the Board.
+     */
+    void resetBoard(){
+
+    }
+
+    /**
      * Moves the tiles in the board array and in activity main
      * @param board
      * @param direction
      */
     void moveTiles(ImageView[][] board, int direction) {
+        //copies layout of the current app
+        final ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+
+        //makes a newTile
+        ImageView newTile = new ImageView(layout.getContext());
+
+        newTile.setID(View.generateViewId());.
 
     }
 
